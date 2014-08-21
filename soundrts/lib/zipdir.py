4dfab4b773ca38998ba49505ea25ed9c10b8e124
soundmud <soundmud@users.noreply.github.com>
2014-08-21 23:45:12 +0200

    refactoring of zipdir.py

--- soundrts/lib/zipdir.py
+++ soundrts/lib/zipdir.py
@@ -1,29 +1,34 @@
 """
 http://stackoverflow.com/questions/3612094/better-way-to-zip-files-in-python-zip-a-whole-directory-with-a-single-command?lq=1
+http://stackoverflow.com/questions/10060069/safely-extract-zip-or-tar-using-python
 """
 
 import os
+import string
 import zipfile
+
+from log import warning
 
 
 def zipdir(target_dir, dest_file):
-    zip = zipfile.ZipFile(dest_file, 'w', zipfile.ZIP_DEFLATED)
+    z = zipfile.ZipFile(dest_file, 'w', zipfile.ZIP_DEFLATED)
     rootlen = len(target_dir) + 1
-    for base, dirs, files in os.walk(target_dir):
-        for file in files:
-            fn = os.path.join(base, file)
-            zip.write(fn, fn[rootlen:])
+    for base, _, filenames in os.walk(target_dir):
+        for n in filenames:
+            p = os.path.join(base, n)
+            z.write(p, p[rootlen:])
 
 def unzipdir(zip_name, dest, overwrite=False):
     if not overwrite and os.path.exists(dest):
         raise Exception("%s already exists!" % dest)
     zfile = zipfile.ZipFile(zip_name)
     for name in zfile.namelist():
-        abs_name = os.path.join(dest, name)
-        (dirname, filename) = os.path.split(abs_name)
-        if not os.path.exists(dirname):
-            os.mkdir(dirname)
-        open(abs_name, "wb").write(zfile.read(name))
+        # This precaution is necessary with Python < 2.7.4 .
+        if ".." not in name and ":" not in name \
+           and name[0] in string.ascii_letters + "_":
+            zfile.extract(name, dest)
+        else:
+            warning("unzipdir: didn't extract %s", name)
 
 
 if __name__ == "__main__":
@@ -31,11 +36,12 @@ if __name__ == "__main__":
     zf = "tmp_zipdirtest.zip"
     if os.path.exists(zf):
         os.remove(zf)
-    zipdir("tests", zf)
+    zipdir("../tests", zf)
     dest = "tmp_tests"
     if os.path.exists(dest):
         shutil.rmtree(dest)
     unzipdir(zf, dest)
+    raw_input("Check %s and %s, then press Enter to continue the test." % (zf, dest))
     try:
         unzipdir(zf, dest)
     except:
