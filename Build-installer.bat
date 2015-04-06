f9cdb1446b8e0a41abd578c3708ba58f8c53a98e
soundmud <soundmud@users.noreply.github.com>
2015-04-06 17:56:02 +0200

    adapted the Build-installer script to Windows 8.1

--- Build-installer.bat
+++ Build-installer.bat
@@ -7,7 +7,7 @@ del %TEMP%\soundrts\dist\soundrts-%MyAppVer%.zip
 "c:\program files\7-zip\7z" a -tzip %TEMP%\soundrts\dist\soundrts-%MyAppVer%.zip %TEMP%\soundrts\build\soundrts-%MyAppVer% -r
 if errorlevel 1 goto fin
 
-"c:\program files\inno setup 5\compil32" /cc %TEMP%\soundrts\build\soundrts.iss
+"c:\program files (x86)\inno setup 5\compil32" /cc %TEMP%\soundrts\build\soundrts.iss
 if errorlevel 1 goto fin
 
 del %TEMP%\soundrts\dist\soundrts-%MyAppVer%-windows.zip
