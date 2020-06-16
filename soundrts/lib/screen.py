d0c82b46e35d8e8a312fe4c3ceb94dea30286b43
soundmud <soundmud@users.noreply.github.com>
2020-06-16 12:55:19 +0200

    migration: now the font will be loaded from the system
    (building an installer will be simpler)

--- soundrts/lib/screen.py
+++ soundrts/lib/screen.py
@@ -8,7 +8,7 @@ from .log import warning
 
 
 pygame.font.init()
-_font = pygame.font.Font("freesansbold.ttf", 12)
+_font = pygame.font.SysFont("arial", 12, bold=True)
 
 def draw_line(color, xy1, xy2):
     pygame.draw.line(_screen, color, xy1, xy2)
