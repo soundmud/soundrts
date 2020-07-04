d090e1dab760733ddec8a8b71611953a50b73702
soundmud <soundmud@users.noreply.github.com>
2020-07-04 15:34:27 +0200

    cleaned up test_training_game.py

--- test_training_game.py
+++ test_training_game.py
@@ -9,9 +9,12 @@ from soundrts import clientmain
 
 if __name__ == "__main__":
     clientmain.init_media()
-#    clientmain.TrainingGame(Map("multi/jl5.txt"), ["test", "easy"], ["random_faction", "random_faction"]).run(speed=20)
-    clientmain.TrainingGame(Map("multi/jl5.txt"),
-                            ["test", "aggressive", "easy"],
-                            ["random_faction", "random_faction", "random_faction"],
-                            ["1", "1", "2"]
-                            ).run(speed=20)
+
+    # map = "multi/pra2.txt"
+    map = "multi/jl5.txt"
+
+    # players = ["test", "easy"], ["random_faction", "random_faction"], ["1", "2"]
+    # players = ["test", "aggressive"], ["random_faction", "random_faction"], ["1", "2"]
+    players = ["test", "aggressive", "easy"], ["random_faction", "random_faction", "random_faction"], ["1", "1", "2"]
+
+    clientmain.TrainingGame(Map(map), *players).run(speed=20)
