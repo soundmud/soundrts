e736b4223910adf0632d12bfcd38d2ae14008d9f
soundmud <soundmud@users.noreply.github.com>
2020-08-04 20:39:50 +0200

    now the client remembers the previously downloaded list of servers

--- soundrts/metaserver.py
+++ soundrts/metaserver.py
@@ -1,15 +1,19 @@
+import os.path
 import urllib.parse
 import urllib.request
 from typing import List
 
 from .lib.log import warning
 from . import msgparts as mp
+from .paths import TMP_PATH
 from .version import SERVER_COMPATIBILITY
 
 # old value used by some features (stats, ...)
 METASERVER_URL = "http://jlpo.free.fr/soundrts/metaserver/"
 
 MAIN_METASERVER_URL = open("cfg/metaserver.txt").read().strip()
+DEFAULT_SERVERS_PATH = "cfg/default_servers.txt"
+RECENT_SERVERS_PATH = os.path.join(TMP_PATH, "recent_servers.txt")
 
 
 def _add_time_and_version(line):
@@ -19,13 +23,12 @@ def _add_time_and_version(line):
 
 
 def _default_servers():
-    lines = open("cfg/default_servers.txt").readlines()
+    lines = open(DEFAULT_SERVERS_PATH).readlines()
     return [_add_time_and_version(line) for line in lines
             if line.strip() and not line.startswith(";")]
 
 
 def servers_list(voice) -> List[str]:
-    servers = _default_servers()
     # The header is an arbitrary string that the metaserver will include
     # in the reply to make sure that the PHP script is executed.
     header = "SERVERS"
@@ -34,14 +37,25 @@ def servers_list(voice) -> List[str]:
     try:
         f = urllib.request.urlopen(servers_url)
         if f.read(len(header)).decode() == header:
-            servers = f.read().decode().split("\n")
+            s = f.read().decode()
+            servers = s.split("\n")
+            try:
+                with open(RECENT_SERVERS_PATH, "w") as t:
+                    t.write(s)
+            except OSError:
+                warning("couldn't save the list of servers")
         else:
-            voice.alert(mp.BEEP)
-            warning(f"the servers list from the metaserver doesn't start with {header}"
-                    " => using the default servers list")
+            raise OSError(f"wrong header")
     except OSError as e:
         voice.alert(mp.BEEP + [str(e)])  # type: ignore
         warning(str(e))
-        warning("couldn't get the servers list from the metaserver"
-                " => using the default servers list")
+        warning("couldn't get the servers list from the metaserver")
+        try:
+            with open(RECENT_SERVERS_PATH) as t:
+                servers = t.read().split("\n")
+            warning(f"using {RECENT_SERVERS_PATH} instead")
+        except OSError:
+            warning(f"couldn't read {RECENT_SERVERS_PATH}")
+            warning(f"using {DEFAULT_SERVERS_PATH} instead")
+            servers = _default_servers()
     return servers
